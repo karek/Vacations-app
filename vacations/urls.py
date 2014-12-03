2d83e3dbdde8558c2c81b8e91f2fc83e04593136
Artur Kozak <artur.kozak@students.mimuw.edu.pl>
2014-12-03 12:45:32 +0100

    Something missing for 'hello world' view.

--- vacations/urls.py
+++ vacations/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from views import hello
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'vacations.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^hello/', hello),
     url(r'^admin/', include(admin.site.urls)),
 )
