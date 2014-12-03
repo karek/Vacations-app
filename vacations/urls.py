a49838ffd0be2a7853e3ec5456b3ef21b521943f
Wojciech Gizynski <gizynski@gmail.com>
2014-12-03 21:54:53 +0100

    Create a Bootstrap index view in the planner app

--- vacations/urls.py
+++ vacations/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from views import hello
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vacations.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^hello/', hello),
+    url(r'^', include('planner.urls', namespace='planner')),
     url(r'^admin/', include(admin.site.urls)),
 )
