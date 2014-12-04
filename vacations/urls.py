1921d2f476f2a35ad5c13b0d045d033b98d73200
Wojciech Gizynski <gizynski@gmail.com>
2014-12-04 08:41:12 +0100

    Fix regexp in app url dispatcher

--- vacations/urls.py
+++ vacations/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', include('planner.urls', namespace='planner')),
+    url(r'^$', include('planner.urls', namespace='planner')),
     url(r'^admin/', include(admin.site.urls)),
 )
