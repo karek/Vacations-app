0f6e2ac72744706251a929c74f8f1d51ea2f290e
Wojciech Gizynski <gizynski@gmail.com>
2015-01-07 21:24:31 +0100

    Change the User creation form to not ask for password

--- planner/admin.py
+++ planner/admin.py
@@ -9,25 +9,25 @@ from planner.models import EmailUser
 class UserCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    # password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    # password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
 
     class Meta:
         model = EmailUser
         fields = ('email', 'first_name', 'last_name')
 
-    def clean_password2(self):
-        # Check that the two password entries match
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
-        return password2
+    # def clean_password2(self):
+    #     # Check that the two password entries match
+    #     password1 = self.cleaned_data.get("password1")
+    #     password2 = self.cleaned_data.get("password2")
+    #     if password1 and password2 and password1 != password2:
+    #         raise forms.ValidationError("Passwords don't match")
+    #     return password2
 
     def save(self, commit=True):
         # Save the provided password in hashed format
         user = super(UserCreationForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
+        # user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
@@ -43,6 +43,7 @@ class UserChangeForm(forms.ModelForm):
     class Meta:
         model = EmailUser
         fields = ('email', 'password', 'first_name', 'last_name', 'is_active', 'is_admin')
+        # fields = ('email', 'first_name', 'last_name', 'is_active', 'is_admin')
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
@@ -71,7 +72,8 @@ class EmailUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2')}
+            # 'fields': ('email', 'first_name', 'last_name', 'password1', 'password2')}
+            'fields': ('email', 'first_name', 'last_name')}
         ),
     )
     search_fields = ('email', 'first_name', 'last_name',)
--- planner/forms.py
+++ planner/forms.py
@@ -4,17 +4,15 @@ from planner.models import EmailUser
 
 
 class RegisterForm(UserCreationForm):
-    # email = forms.EmailField(required=True)
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
 
     class Meta:
         model = EmailUser
-        fields = ("email", "first_name", "last_name", "password1", "password2")
+        fields = ("email", "first_name", "last_name")
 
     def save(self, commit=True):
         user = super(RegisterForm, self).save(commit=False)
-        # user.email = self.cleaned_data["email"]
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
         if commit:
