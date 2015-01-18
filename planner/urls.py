52eef696284c923c77c9b90deb65d17da376a851
Artur Kozak <artur.kozak@students.mimuw.edu.pl>
2015-01-18 02:20:39 +0100

    Refactor booking vacations to planning absences.

--- planner/urls.py
+++ planner/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns(
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
-    url(r'^book-vacation/$', views.BookVacationView.as_view(), name='book-vacation')
+    url(r'^plan-absence/$', views.PlanAbsenceView.as_view(), name='plan-absence')
 )
 
