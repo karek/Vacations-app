ff2d4a7da629f79627af62f22d2b65c9898e61e5
Wojciech Gizynski <gizynski@gmail.com>
2015-01-29 13:11:02 +0100

    Clean code a little, add semicolons to javascript, minor refactor

--- planner/static/planner/js/selection.js
+++ planner/static/planner/js/selection.js
@@ -1,14 +1,13 @@
 function selectf(begin, end) {
 	// console.log("selectf")
-	var range1 = { begin: begin, end: end }
+	var range1 = { begin: begin, end: end };
 	$(".s_range").each(function(index) {
 		var range2 = { begin: moment($(this).attr("s_begin")), end: moment($(this).attr("s_end"))}
 		if (!if_disjoint(range1, range2)) {
-			range1 = join_ranges(range1, range2)
-			this.remove()
+			range1 = join_ranges(range1, range2);
+			this.remove();
 		}
-			
-	})
+	});
 
     var begin_str = range1.begin.format('YYYY-MM-DD');
     var end_str = range1.end.format('YYYY-MM-DD');
@@ -28,12 +27,11 @@ function selectf(begin, end) {
     	// console.log("comp")
      	return ($(b).attr("s_begin") < $(a).attr("s_begin")) ?  1 : -1
      }
-    $('#absence_select li').sort(comp).appendTo('#absence_select');
-
+    $('#absence_select a').sort(comp).appendTo('#absence_select');
 }
 
 function unselectf(view, jsEvent) {
-	console.log("unselectf")
+	console.log("unselectf");
 	$('#yourCalendar').fullCalendar('unselect');
 }
 
@@ -44,35 +42,32 @@ function unselectf(view, jsEvent) {
 // if_disjoint :: { begin: moment, end: moment} , {begin: moment, end: moment} -> bool 
 function if_disjoint(range1, range2) {
 	if (range1.begin > range2.begin)
-		return if_disjoint(range2, range1) // now we now that range1.begin <= range2.begin 
-	if (range1.end < range2.begin)
-		return true
-	else
-		return false	
+		return if_disjoint(range2, range1); // now we know that range1.begin <= range2.begin
+	return range1.end < range2.begin;
 }
 
 // join_ranges :: { begin: moment, end: moment} , {begin: moment, end: moment} -> {moment, moment} 
 function join_ranges(range1, range2) {
 	if (range1.begin > range2.begin)
-		return join_ranges(range2, range1) // now we now that range1.begin <= range2.begin 	
+		return join_ranges(range2, range1); // now we know that range1.begin <= range2.begin
 	if (range1.end <= range2.end) 
-		return {begin: range1.begin, end: range2.end } // 2.
+		return {begin: range1.begin, end: range2.end }; // 2.
 	else
-		return {begin: range1.begin, end: range1.end } // 1.
+		return {begin: range1.begin, end: range1.end }; // 1.
 }
 
 $(document).on('click', '.btn-remove', function(){
-	console.log()
+	console.log('removing selected range');
 	// if someone has more stupid idea to refresh all selected days, please show me
 	$(this).parent().remove()
-	$('#calendar').fullCalendar( 'next' )
-	$('#calendar').fullCalendar( 'prev' )
+	$('#calendar').fullCalendar('next');
+	$('#calendar').fullCalendar('prev');
 	
 	$(".s_range").each(function(index) {
-		console.log($(this).attr("s_begin"))
-		console.log($(this).attr("s_end"))
-		m1 = moment($(this).attr("s_begin"))
-		m2 = moment($(this).attr("s_end"))
+		console.log($(this).attr("s_begin"));
+		console.log($(this).attr("s_end"));
+		m1 = moment($(this).attr("s_begin"));
+		m2 = moment($(this).attr("s_end"));
 		$('#calendar').fullCalendar('select', m1, m2)
 	})
 
