7d1366bad9242d5de389b8567d5a44c884547484
karek <Krzysztof.kiljan@student.uw.edu.pl>
2015-05-11 01:13:07 +0200

    Logged user is displayed same as any other user

--- planner/static/planner/js/ajaxes.js
+++ planner/static/planner/js/ajaxes.js
@@ -104,10 +104,10 @@ function calcSorted(data) {
     }
 
 // In future functions sort it in some other way
-    global_users_sorted = global_users_sorted.filter(
-        function (a) {
-            return a.id != global_logged_user_id;
-        });
+//    global_users_sorted = global_users_sorted.filter(
+//        function (a) {
+//            return a.id != global_logged_user_id;
+//        });
 
     sortAndSaveUsersOrder(function (a, b) {
 
