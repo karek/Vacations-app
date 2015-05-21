3792abcc316eca2b4e6d9f34f39223c3cbe9358b
Artur Kozak <artur.kozak@students.mimuw.edu.pl>
2015-05-21 12:49:59 +0200

    missing migration

---
+++ planner/migrations/0023_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('planner', '0021_auto_20150521_1201'),
+        ('planner', '0022_manual_holiday_calendar'),
+    ]
+
+    operations = [
+    ]
