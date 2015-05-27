ee3a634b0de7c6e07df96563beaa71351e532bb2
Wojciech Gizynski <gizynski@gmail.com>
2015-05-28 00:14:36 +0200

    Fix a problem with user registering without selecting team

--- planner/forms.py
+++ planner/forms.py
@@ -51,6 +51,7 @@ class UserChangeForm(forms.ModelForm):
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
+    team = forms.ModelChoiceField(required=True, queryset=Team.objects.all(), empty_label=None)
 
     class Meta:
         model = EmailUser
