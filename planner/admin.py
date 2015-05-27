37b9f5c087d1dd2058f09b82b45534368237d6f9
Wojciech Gizynski <gizynski@gmail.com>
2015-05-27 22:27:39 +0200

    Allow filtering employees by is_teamleader field

--- planner/admin.py
+++ planner/admin.py
@@ -16,7 +16,7 @@ class EmailUserAdmin(UserAdmin):
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
     list_display = ('email', 'first_name', 'last_name', 'is_admin', 'team', 'is_teamleader')
-    list_filter = ('is_admin',)
+    list_filter = ('is_admin', 'is_teamleader')
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name', 'team', 'holidays', 'is_teamleader')}),
